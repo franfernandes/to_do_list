b5d1beb94a802b8fb96af595a32cd121f9835a5d
Francielle Fernandes Rodrigues <francielle.rodrigues@taglivros.com.br>
2023-11-10 00:16:21 -0300

    criação de teste da rota  detalhe_task

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+from .models import Task
+
+class TaskAPITestCase(TestCase):
+    def setUp(self):
+        self.task = Task.objects.create(titulo='Teste', descricao='Testes Unitarios para uma tarefa')
+
+    def test_detalhe_task(self):
+        response = self.client.get(reverse('detalhe_task', args=[self.task.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Teste')
